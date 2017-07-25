a551e698277dfa89279c98c804f126b57c1f48be
miclovich <vicmiclovich@gmail.com>
2017-07-25 23:51:09 +0300

    tidy up things... prep for discussion

--- src/app/album-detail.component.ts
+++ src/app/album-detail.component.ts
@@ -1,26 +1,32 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Album } from './album';
 import { Track } from './track';
-import { Router, ActivatedRoute } from '@angular/router';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { Location } from '@angular/common';
 import { AlbumService } from './album.service';
 import 'rxjs/add/operator/switchMap';
 
 
 @Component({
-	selector:'album-details',
+	selector: 'album-details',
 	templateUrl: './album-detail.component.html',
 	styleUrls: ['./album-detail.component.css'],
 	providers: [AlbumService]
 })
 
-export class AlbumDetailComponent {//implements OnInit  {
-	// @Input() track: Track;
+export class AlbumDetailComponent implements OnInit  {
+	
+	tracks: Track[] = [];// we are fetching tracks belonging to an album
 
-	constructor(private router: Router) {}
+	constructor(private albumService: AlbumService,
+		private route: ActivatedRoute, private location: Location) {}
 
-	// ngOnInit(): void {
-	// 	this.router.paramMap
-	// 		.switchMap((params: ParamMap) => this.albumService.getDetail(params.get('id')))
-	// 		.subscribe( album => this.album = album);
-	// }
+	ngOnInit(): void {
+		this.route.paramMap
+			.switchMap((params: ParamMap) => this.albumService.getTracks(params.get('id')))
+			.subscribe(tracks => {
+				console.log(tracks, 'tracks');
+				// this.tracks = tracks
+			});
+	}
 }
