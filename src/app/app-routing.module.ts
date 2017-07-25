4cc6fba59e3c5c4a03fd2a19f40a75a11860c4ba
miclovich <vicmiclovich@gmail.com>
2017-07-25 22:33:51 +0300

    cleanup and added new route to dashboard plus a redirect

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,18 @@
-import { NgModule }             from '@angular/core';
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-import { AppComponent }   from './app.component';
+import { AlbumDetailComponent } from './album-detail.component';
+import { DashboardComponent } from './dashboard.component';
+import { AppComponent } from './app.component';
 
 const routes: Routes = [
-{path: '', component:AppComponent}];
-// { path: 'album_tracks/:album_id', component: AlbumDetailComponent }];
+	{ path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+	{ path: 'dashboard', component: DashboardComponent },
+	{ path: 'album_tracks/:id', component: AlbumDetailComponent }
+]
 
 @NgModule({
-	imports: [ RouterModule.forRoot(routes) ],
-  	exports: [ RouterModule ]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule]
 })
 
-export class AppRoutingModule {}
+export class AppRoutingModule { }
