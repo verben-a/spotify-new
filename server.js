74e268ff729c516bb17e28a66690c945168bf108
verben-a <alina.verbenchuk@gmail.com>
2017-08-03 22:33:42 +0100

    update

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // server.js
 const express = require('express');
 const app = express();
+const path = require('path');
 
 const forceSSL = function() {
   return function (req, res, next) {
@@ -19,7 +20,14 @@ app.use(forceSSL());
 
 // Run the app by serving the static files
 // in the dist directory
+
 app.use(express.static(__dirname + '/dist'));
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+
+
+
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 8080);
